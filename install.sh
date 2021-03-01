f89947893be17840f5753b00bfdc71257ca57353
Admcauss-alt <77009854+Admcauss-alt@users.noreply.github.com>
2021-03-01 16:30:35 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+pkg upgrade
+cd
+git clone https://github.com/Admcauss-alt/Acauzz-
+clear
+cd ./Acauzz.
+bash install.sh
+cd
+cd ./Acauzz-
+npm start
+
+echo "[*] obrigado por utilizar o bot \"Caussbot\" causs agradece"
