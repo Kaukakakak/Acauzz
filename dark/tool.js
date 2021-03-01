3a841e3dbb1c378433bbdc7d8ea9b80076caf4ae
Admcauss-alt <77009854+Admcauss-alt@users.noreply.github.com>
2021-03-01 09:11:43 -0300

    Add files via upload

---
+++ dark/tool.js
@@ -0,0 +1,23 @@
+const tool = () => { 
+	return `           
+──────────────────
+*TOOL HACKER TERMUX*
+──────────────────
+        『 CAUSS BOT 』
+──────────────────
+apt-get install python3
+
+apt-get install git
+
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+
+pip3 install requests
+
+cd Cam-Hackers
+
+python3 cam-hackers.py
+──────────────────
+        『 CAUSS BOT 』
+──────────────────`
+}
+exports.tool = tool
